fd1a548a49931cdc3959cf769ff2a73c2ddf92a6
lhbtran2003 <lhbtran2003@gmail.com>
2024-06-25 19:32:49 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Col, Container, Row, ListGroup, Form, InputGroup } from "react-bootstrap";
 import "./App.css";
 import "react-bootstrap-icons"
-import { Apple, Backpack3, BagCheck, Bell, Calendar2Check, ChatRightQuote, Gear, GridFill, SearchHeart, Wallet } from "react-bootstrap-icons";
+import { Apple, Backpack3, BagCheck, Bell, Calendar2Check, ChatRightQuote, Download, Gear, GridFill, QuestionCircle, SearchHeart, Wallet } from "react-bootstrap-icons";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,45 +10,52 @@ function App() {
   return (
     <div className="p-0 m-0 outfit-uniquifier " style={{ height: "100vh" }}>
       <Row style={{ height: "100vh" }}>
-        <Col md={2} className="ps-8 pt-9">
-          <h1 className="fs-4 ps-3 text-cyan-600">Sale.</h1>
+        <Col md={2} className="pt-9 pe-0 d-flex flex-column">
+          <h1 className="fs-4 ps-4 text-cyan-600">Sale.</h1>
           <ListGroup className="pt-4">
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none rounded-none  mb-2 pe-0 ps-10 d-flex gap-2 align-items-center  actived ">
               <GridFill />
               Dashboard
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <Backpack3 />
               Lab Test
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <Calendar2Check />
               Appointment
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <BagCheck />
               Medicine Order
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <ChatRightQuote />
               Message
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <Wallet />
               Payment
             </ListGroup.Item>
-            <ListGroup.Item className="border-0 mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover">
+            <ListGroup.Item className="border-none mb-2 pe-0 ps-10 d-flex gap-2 align-items-center title-hover unactive">
               <Gear />
               Settings
             </ListGroup.Item>
           </ListGroup>
+          <ListGroup className="mt-auto ">
+            <ListGroup.Item className="border-0  mb-2 pe-0 ps-4 d-flex gap-2 align-items-center title-hover text-center">
+              <QuestionCircle />
+              Help
+            </ListGroup.Item>
+          </ListGroup>
         </Col>
+
         <Col md={10} className="bg-slate-100 pt-9 ps-5 pe-5">
           <Row>
             <Col md={7}>
-              <InputGroup>
-                <InputGroup.Text>
-                  <SearchHeart className="border-none rounded-none" />
+              <InputGroup className="border border-1">
+                <InputGroup.Text className="border-none bg-white">
+                  <SearchHeart className="text-secondary" />
                 </InputGroup.Text>
                 <Form.Control
                   type="text"
@@ -59,8 +66,7 @@ function App() {
             </Col>
             <Col md={3}></Col>
             <Col className="d-flex justify-end">
-              <Bell className="bg-white fs-5 h-8 w-8 p-2 me-3 text-secondary">
-              </Bell>
+              <Bell className="bg-white fs-5 h-8 w-8 p-2 me-3 text-secondary"></Bell>
               <img
                 className="h-8 w-8 rounded-lg"
                 src="https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-1/419589282_1792314451191960_3650239086196055711_n.jpg?stp=dst-jpg_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeGrRfjKju_UZHhxTSWpDfFBO5BT8EwMtfk7kFPwTAy1-ajJiq1kxpsFCCIx6jipAiM3LLwrTWjD7uf1HFTiO3eT&_nc_ohc=hCPJKOFyD9kQ7kNvgHQLugz&_nc_ht=scontent.fsgn2-5.fna&oh=00_AYDvIXg7PWQkx2bT8Xneb3Oba7imV_IhIGs_oF3NUX4CEA&oe=6680564A"
